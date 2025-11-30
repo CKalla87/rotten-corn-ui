79ec26e3d18c902ad4d8cb3b4a5cbe9c0dfa93ea
Christopher Kalla <christopherkalla@macbookpro.lan>
2025-11-29 21:34:57 -0700

    adding login auth code

---
+++ src/pages/auth/login/Login.tsx
@@ -0,0 +1,103 @@
+import { useState, useEffect } from 'react';
+import './Login.sass';
+import { FaArrowRight } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import Input from '@components/input/Input';
+import Button from '@components/button/Button';
+import { authService } from '@services/api/auth/auth.service';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [keepLoggedIn, setKeepLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [alertType, setAlertType] = useState('');
+  const [user, setUser] = useState<any>(null);
+  const navigate = useNavigate();
+
+  const loginUser = async (event: React.FormEvent<HTMLFormElement>) => {
+    setLoading(true);
+    event.preventDefault();
+    try {
+      const result = await authService.signIn({ username, password });
+      // 1 - set logged in to true in local storage
+      // 2 - set username in local storage
+      // 3 - dispatch user to redux
+      setKeepLoggedIn(keepLoggedIn);
+      setUser(result.data.user);
+      setHasError(false);
+      setAlertType('alert-success');
+    } catch (error: any) {
+      setLoading(false);
+      setHasError(true);
+      setAlertType('alert-error');
+      setErrorMessage(error?.response?.data?.message);
+    }
+  };
+
+  useEffect(() => {
+    if (loading && !user) return;
+    if (user) {
+      navigate('/app/social/streams');
+    }
+  }, [loading, user, navigate]);
+
+  return (
+    <div className="auth-inner">
+      {hasError && errorMessage && (
+        <div className={`alerts ${alertType}`} role="alert">
+          {errorMessage}
+        </div>
+      )}
+      <form className="auth-form" onSubmit={loginUser}>
+        <div className="form-input-container">
+          <Input
+            id="username"
+            name="username"
+            type="text"
+            value={username}
+            labelText="Username"
+            placeholder="Enter Username"
+            style={{ border: `${hasError ? '1px solid #fa9b8a' : ''}` }}
+            handleChange={(event) => setUsername(event.target.value)}
+          />
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            value={password}
+            labelText="Password"
+            placeholder="Enter Password"
+            style={{ border: `${hasError ? '1px solid #fa9b8a' : ''}` }}
+            handleChange={(event) => setPassword(event.target.value)}
+          />
+          <label className="checkmark-container" htmlFor="checkbox">
+            <Input 
+              id="checkbox" 
+              name="checkbox" 
+              type="checkbox" 
+              value={keepLoggedIn}
+              handleChange={() => setKeepLoggedIn(!keepLoggedIn)}
+            />
+            Keep me signed in
+          </label>
+        </div>
+        <Button 
+          label={loading ? 'SIGNIN IN PROGRESS...' : 'SIGNIN'}
+          className="auth-button button"
+          disabled={!username || !password}
+        />
+        <Link to={'/forgot-password'}>
+          <span className="forgot-password">
+            Forgot password? <FaArrowRight className="arrow-right" />
+          </span>
+        </Link>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
+
